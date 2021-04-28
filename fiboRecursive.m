e3fbcd498dd8838b9a740bbaebaebbda06c5efa8
Chloe Lehnert <chloe.lehnert@gmail.com>
2021-04-28 14:44:38 -0500

    second step in making a merge conflict

--- fiboRecursive.m
+++ fiboRecursive.m
@@ -1,6 +1,8 @@
 function [f] = fiboRecursive(n)
+%a different comment
     if n == 1 || n == 2
         f = 1;
+        %more changes
     else
         f = fiboRecursive(n-1) + fiboRecursive(n-2);
     end
